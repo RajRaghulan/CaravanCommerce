2e84272f87e1c79139556ba3e284042482a812b5
Raj Raghulan <rajraghulan@gmail.com>
2021-01-11 19:46:28 -0800

    Storage

---
+++ public/app.js
@@ -0,0 +1,55 @@
+
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
+
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+/// Sign in event handlers
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+    } else {
+        // not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = '';
+    }
+});
+// Attempting React
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import GoogleLogin from 'react-google-login';
+// // or
+// import { GoogleLogin } from 'react-google-login';
+
+
+// const responseGoogle = (response) => {
+//   console.log(response);
+// }
+
+// ReactDOM.render(
+//   <GoogleLogin
+//     clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+//     buttonText="Login"
+//     onSuccess={responseGoogle}
+//     onFailure={responseGoogle}
+//     cookiePolicy={'single_host_origin'}
+//   />,
+//   document.getElementById('googleButton')
+// );

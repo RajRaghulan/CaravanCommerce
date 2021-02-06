6b4a4bfd9ffa289a9355586ec365dbbc5b789bdf
Vijay Sundhar <vsundhar@macbook-pro-5.lan>
2021-02-06 14:16:49 -0800

    Updated Site

---
+++ public/assets/js/index.js
@@ -0,0 +1,9 @@
+const functions = require("firebase-functions");
+
+document,getElementById('loginForm').addEventListener('', login);
+
+function login(e) {
+  e.preventDefault();
+  console.log('login');
+  
+}

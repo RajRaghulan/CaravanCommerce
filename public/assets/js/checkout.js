b4e04cb4d98e9911b3a73bfe658c8aa01166b620
Raj Raghulan <rajraghulan@gmail.com>
2021-02-13 19:11:55 -0800

    Basic Website Changes

--- public/assets/js/checkout.js
+++ public/assets/js/checkout.js
@@ -1,44 +1,28 @@
-var cardDrop = document.getElementById('card-dropdown');
-var activeDropdown;
-cardDrop.addEventListener('click',function(){
-  var node;
-  for (var i = 0; i < this.childNodes.length-1; i++)
-    node = this.childNodes[i];
-    if (node.className === 'dropdown-select') {
-      node.classList.add('visible');
-       activeDropdown = node; 
-    };
-})
-
-window.onclick = function(e) {
-  console.log(e.target.tagName)
-  console.log('dropdown');
-  console.log(activeDropdown)
-  if (e.target.tagName === 'LI' && activeDropdown){
-    if (e.target.innerHTML === 'Master Card') {
-      document.getElementById('credit-card-image').src = 'https://dl.dropboxusercontent.com/s/2vbqk5lcpi7hjoc/MasterCard_Logo.svg.png';
-          activeDropdown.classList.remove('visible');
-      activeDropdown = null;
-      e.target.innerHTML = document.getElementById('current-card').innerHTML;
-      document.getElementById('current-card').innerHTML = 'Master Card';
-    }
-    else if (e.target.innerHTML === 'American Express') {
-         document.getElementById('credit-card-image').src = 'https://dl.dropboxusercontent.com/s/f5hyn6u05ktql8d/amex-icon-6902.png';
-          activeDropdown.classList.remove('visible');
-      activeDropdown = null;
-      e.target.innerHTML = document.getElementById('current-card').innerHTML;
-      document.getElementById('current-card').innerHTML = 'American Express';      
-    }
-    else if (e.target.innerHTML === 'Visa') {
-         document.getElementById('credit-card-image').src = 'https://dl.dropboxusercontent.com/s/ubamyu6mzov5c80/visa_logo%20%281%29.png';
-          activeDropdown.classList.remove('visible');
-      activeDropdown = null;
-      e.target.innerHTML = document.getElementById('current-card').innerHTML;
-      document.getElementById('current-card').innerHTML = 'Visa';
-    }
-  }
-  else if (e.target.className !== 'dropdown-btn' && activeDropdown) {
-    activeDropdown.classList.remove('visible');
-    activeDropdown = null;
-  }
-}
+const stripe = require('stripe')('sk_test_51Hq3XbDZD8uKIYCGdPern6YFjR4HxxIJTk4qKJoppiziYLTIE4R3KM3dZ7C9kHqbDvZrH6t2KkRWFHyt1MY4zIF600BOoWXDNO');
+const express = require('express');
+const app = express();
+app.use(express.static('.'));
+const YOUR_DOMAIN = 'http://localhost:4242';
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'Stubborn Attachments',
+            images: ['https://i.imgur.com/EHyR2nP.png'],
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}/success.html`,
+    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+  });
+  res.json({ id: session.id });
+});
+app.listen(4242, () => console.log('Running on port 4242'));

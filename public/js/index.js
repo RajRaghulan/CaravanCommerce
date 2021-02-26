d169b3a075ddef37f8b0ecc9ce57a626b80d9069
Raj Raghulan <rajraghulan@gmail.com>
2021-02-26 03:38:36 -0800

    Website Refresh

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDss7Fwh_9AEW-C-H4yoaEZ3VDOloFLxZ8",
+    authDomain: "caravan-commerce.firebaseapp.com",
+    databaseURL: "https://caravan-commerce-default-rtdb.firebaseio.com",
+    projectId: "caravan-commerce",
+    storageBucket: "caravan-commerce.appspot.com",
+    messagingSenderId: "1054138601545",
+    appId: "1:1054138601545:web:5a5ece43e0df4998882f98",
+    measurementId: "G-TYPGWT8JKR"
+ };
+    
+ firebase.initializeApp(firebaseConfig);
